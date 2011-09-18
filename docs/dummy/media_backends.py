9bd269214614e618edb2fcfbea0d30d3b0fd7e09
philomat <philomat@popkultur.net>
2011-09-18 18:33:43 -0400

    Doc fixes

--- docs/dummy/media_backends.py
+++ docs/dummy/media_backends.py
@@ -1,10 +1,19 @@
 from media_tree.media_backends import MediaBackend
 from media_tree import media_types
 
+
 class DummyBackend(MediaBackend):
-    """
-    Media backend for easy_thumbnails support. 
-    """
 
     SUPPORTED_MEDIA_TYPES = (media_types.SUPPORTED_IMAGE,)
 
+    @staticmethod
+    def check_conf():
+        pass
+
+    @staticmethod
+    def get_thumbnail(source, options):
+        pass
+
+    @staticmethod
+    def get_valid_thumbnail_options():
+        return ()
