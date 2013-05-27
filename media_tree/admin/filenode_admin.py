14daa6218e6313afb63094f6dd834e3e46fff8ee
Erlend Dalen <erlend.dalen@gmail.com>
2013-05-27 15:02:31 +0200

    Issue #25: Only folders for the current site.
    Also filter folders that has been registerred in the current site.

--- media_tree/admin/filenode_admin.py
+++ media_tree/admin/filenode_admin.py
@@ -140,8 +140,11 @@ class FileNodeAdmin(MPTTModelAdmin):
         if db_field.name == 'parent' and issubclass(db_field.rel.to, FileNode):
             # overriding formfield_for_dbfield, thus bypassign both Django's and mptt's
             # formfield_for_foreignkey method, and also preventing Django from wrapping
-            # field with RelatedFieldWidgetWrapper ("add" button resulting in a file add form) 
-            valid_targets = FileNode.tree.filter(**db_field.rel.limit_choices_to)
+            # field with RelatedFieldWidgetWrapper ("add" button resulting in a file add form)
+
+            # Issue 25 - Only allow folders registerred in the current site.
+            valid_targets = FileNode.tree.filter(**db_field.rel.limit_choices_to).filter(
+                                                 site=Site.objects.get_current())
             request = kwargs['request']
             node = get_request_attr(request, 'save_node', None)
             if node:
