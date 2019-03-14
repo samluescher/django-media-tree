3eb6345faaf57e2fbe35ca431d4d133f950f2b5f
Samuel Luescher <samuel@formlabs.com>
2019-03-14 16:29:12 +0100

    Update for pip 10+

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ import os
 import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-from pip.req import parse_requirements
-
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt', session=False)
 
